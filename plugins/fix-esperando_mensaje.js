0a111d21c6aff69a59f283d56bdf9979a3d639de
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-16 08:37:13 -0500

    Actualizar fix-esperando_mensaje.js

--- plugins/fix-esperando_mensaje.js
+++ plugins/fix-esperando_mensaje.js
@@ -30,7 +30,7 @@ const handler = async (m, { conn, usedPrefix }) => {
     console.error('> Error Verifique La Carpeta De Session:', err);
     await conn.sendMessage(m.chat, {text: '> Verifique Que Sea Tal Cual La Carpeta De Session'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `> AntiBug X Yallico 🇦🇱\n\n> Si El Bot No Responde Aún , Le Recomiendo Hacer Un Pequeño Spam De 3 Veces Ejecutando El Siguiente Comando: .s`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `> AntiBug X YerTX2🇦🇱\n\n> Si El Bot No Responde Aún , Le Recomiendo Hacer Un Pequeño Spam De 3 Veces Ejecutando El Siguiente Comando: .s`}, {quoted: m});
 };
 handler.help = ['AntiBug1'];
 handler.tags = ['main'];
