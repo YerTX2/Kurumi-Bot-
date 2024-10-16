8b489a43b6c8c37969ce582951d5c7e1fed09d31
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-16 14:14:41 -0500

    Actualizar pago-staff.js

--- plugins/pago-staff.js
+++ plugins/pago-staff.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, text, command}) => {
   const yh = global.staff;
   const url = yh[Math.floor(Math.random() * yh.length)];
-  conn.sendMessage(m.chat, {image: {url: url}, caption: '*Te Presentamos Al Staff De Tanjiro Bot 👾*\n\n*Creador Oficial :*\n- *Yallico 🤖  / +51 926 956 637*\n\n*Vendedores Oficiales :*\n- *Nare 👸 / +51 971 060 913*\n- *Nat 👸 / +52 417 206 4415*\n- *Lexus 🤴 / +505 8858 6015*\n- *Mayra 👸 / +54 9 11 6122-5453*\n- *Mari 👸 / +54 9 11 6395-9270*\n\n*Grupo Oficial :* https://chat.whatsapp.com/D8b5JZEIfLF3YZiCDP1cAp'}, {quoted: m});
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*hola 🌟*\n\n*Creador Oficial :*\n- *YerTX2 🇦🇱*\n\n*Grupo Oficial :* https://chat.whatsapp.com/EfnhQvzXQWW940w73UnRBh'}, {quoted: m});
 };
 handler.command = /^(staff)$/i;
 handler.tags = ['main'];
