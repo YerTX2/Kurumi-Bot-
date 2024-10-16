b9cffbcd95f3d646d22f2af135bf9a258d58b343
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-16 14:21:23 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -8,7 +8,7 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let name = await conn.getName(who)
 let edtr = `@${m.sender.split`@`[0]}`
 
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;  ૈYallico 🍃\nNICKNAME:🇦🇱 dv.yer\nORG: YER👾\nTITLE:soft\nitem1.TEL;waid=51 907 376 960 :+51 907 376 960 \nitem1.X-ABLabel:📞 WhatsApp Owner\nitem2.URL:https://github.com/YerTX2\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: oroscoyerson2019@gmail.com\nitem3.X-ABLabel:💌 Correo soporte\nitem4.ADR:;;🇦🇱 de algún lugar ;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel: Localización 🫧\nBDAY;value=date:👾 09-02-2006\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;  ZxYerSixZx 🍃\nNICKNAME:🇦🇱 dv.yer\nORG: YER👾\nTITLE:soft\nitem1.TEL;waid=51 907 376 960 :+51 907 376 960 \nitem1.X-ABLabel:📞 WhatsApp Owner\nitem2.URL:https://github.com/YerTX2\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: oroscoyerson2019@gmail.com\nitem3.X-ABLabel:💌 Correo soporte\nitem4.ADR:;;🇦🇱 de algún lugar ;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel: Localización 🫧\nBDAY;value=date:👾 09-02-2006\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: packname, contacts: [{ vcard }] }}, { quoted: fkontak })
 let caption = `*👋 Hola ${edtr}, este es el contacto de mi creador 🇦🇱*`
     await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) })
