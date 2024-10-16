581aa57074d9433d4cf65be398a84552f96a6bbd
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-16 14:18:57 -0500

    Actualizar pago-staff.js

--- plugins/pago-staff.js
+++ plugins/pago-staff.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, text, command}) => {
   const yh = global.staff;
   const url = yh[Math.floor(Math.random() * yh.length)];
-  conn.sendMessage(m.chat, {image: {url: url}, caption: '*hola 🌟*\n\n*Creador Oficial :*\n- *YerTX2 🇦🇱*\n\n*Grupo Oficial :* https://chat.whatsapp.com/EfnhQvzXQWW940w73UnRBh'}, {quoted: m});
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*hola 🌟*\n\n*Creador Oficial :*\n- *YerTX2 🇦🇱*\51 907 376 960 n\n*Grupo Oficial :* https://chat.whatsapp.com/EfnhQvzXQWW940w73UnRBh'}, {quoted: m});
 };
 handler.command = /^(staff)$/i;
 handler.tags = ['main'];
