0c2baa426416f0241c429ad71692612ef6b62834
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-16 14:24:15 -0500

    Actualizar mods-autoadmin.js

--- plugins/mods-autoadmin.js
+++ plugins/mods-autoadmin.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
   await m.react(done)
    m.reply('👾 *¡YA TE DI ADM MI JEFE!*', m, rcanal);
     let nn = conn.getName(m.sender);
-     conn.reply('51926956637@s.whatsapp.net', `👾 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+     conn.reply('09900000@s.whatsapp.net', `👾 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
   } catch {
     m.reply('👾 Ocurrio un error.');
   }
