c2755d741c5387f5412b9ee16d2d01615cc1a973
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-10-23 21:01:44 -0500

    Actualizar menu-descargas.js

--- plugins/menu-descargas.js
+++ plugins/menu-descargas.js
@@ -21,7 +21,7 @@ const defaultMenu = {
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
-'dl': 'menudescarga',
+'dl': 'menudl',
 }
 let img = 'https://i.ibb.co/PGJGjQk/file.jpg'
 
